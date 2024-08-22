9438d0aad11719812ddfc691fc1f9a9cb598224e
aleperfect <143966062+aleperfect@users.noreply.github.com>
2024-08-22 18:02:57 -0500

    dssdf

--- plugins/sticker-slap.js
+++ plugins/sticker-slap.js
@@ -19,7 +19,7 @@ const handler = async (m, { conn, args, usedPrefix, command }) => {
   try {
     let name;
     if (who === m.chat) {
-      name = "Readz Aleizn";
+      name = "𝗚𝗛𝗢𝗦𝗧 𝗕𝗢𝗧";
     } else {
       name = conn.getName(who);
     }
